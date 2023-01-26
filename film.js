8400d71b07982031ef592a817668e7883619a652
Bob Smith <wasadmin@webpagesolutions.com>
2023-01-26 11:22:10 -0500

    fixed aligning issues

--- film.js
+++ film.js
@@ -1,6 +1,7 @@
 let title;
 let planets;
 let characters;
+let starshipsUl;
 
 // Runs on page load
 addEventListener('DOMContentLoaded', () => {
@@ -9,6 +10,7 @@ addEventListener('DOMContentLoaded', () => {
   characters = document.querySelector('#characters>ul');
   planetsUl = document.querySelector('#planets>ul');
   charactersUl = document.querySelector('#characters>ul');
+  starshipsUl = document.querySelector('#starships>ul');
   const sp = new URLSearchParams(window.location.search)
   const id = sp.get('id')
   fetchFilm(id)
@@ -30,21 +32,15 @@ const renderFilm = film => {
   console.log (`planets are : ${film.planets}, Characters are: ${film.characters}`)
   document.title = `SWAPI - ${film?.title}`;  // Just to make the browser tab say their name
   title.textContent = film?.title;
-  const planetsList = film?.planets.map(planetUrl => `
-       <li>
-           <a href="/planets.html?id=${getPlanetIdFromUrl(planetUrl)}">
-           ${getPlanetIdFromUrl(planetUrl)}
-       </li>
-    `);
+  const planetsList = film?.planets.map(planetUrl => `<li><a href="/planet.html?id=${getPlanetIdFromUrl(planetUrl)}">${getPlanetIdFromUrl(planetUrl)}</li>`);
+    console.log(planetsList);
     planetsUl.innerHTML = planetsList.join("");
 
-  const charactersList = film?.characters.map(characterUrl => `
-        <li>
-            <a href="/person.html?id=${getCharacterIdFromUrl(characterUrl)}">
-            ${getCharacterIdFromUrl(characterUrl)}
-        </li>
-    `);
+  const charactersList = film?.characters.map(characterUrl => `<li><a href="/person.html?id=${getCharacterIdFromUrl(characterUrl)}">${getCharacterIdFromUrl(characterUrl)}</li>`);
     charactersUl.innerHTML = charactersList.join("");
+
+    const starshipsList = film?.starships.map(shipUrl => `<li><a href="/starshipInfo.html?id=${getShipIdFromUrl(shipUrl)}">${getShipIdFromUrl(shipUrl)}</li>`);
+    starshipsUl.innerHTML = starshipsList.join("");
 }
 
 const getIdFromUrl = (entityName, url) => {
@@ -55,3 +51,4 @@ const getIdFromUrl = (entityName, url) => {
 }
 const getPlanetIdFromUrl = url => getIdFromUrl("planets", url)
 const getCharacterIdFromUrl = url => getIdFromUrl("people", url)
+const getShipIdFromUrl = url => getIdFromUrl("starships", url)
